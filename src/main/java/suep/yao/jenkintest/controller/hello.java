390f917571032c6897b86b47457aaf2e01959f5b
Yaoh <369770384@qq.com>
2023-04-03 13:39:28 +0800

    测试Jenkins是否正常运行，本次war运行环境jdk17，端口8848

---
+++ src/main/java/suep/yao/jenkintest/controller/hello.java
@@ -0,0 +1,14 @@
+package suep.yao.jenkintest.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+    public class hello {
+
+        @RequestMapping("/hello")
+        public String hello(){
+            return "hello";
+        }
+}
---
+++ src/test/java/suep/yao/jenkintest/JenkinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package suep.yao.jenkintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
